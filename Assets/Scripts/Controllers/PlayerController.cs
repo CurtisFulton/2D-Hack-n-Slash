2f65130afba7758b61b95eff34486ef04b438df8
Maitland Marshall <maitlandmarshallapps@gmail.com>
2018-08-18 10:22:26 +1000

    sprites, MVC scaffolding for player, folder scaffolding

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController {
+
+    MovementComponent movementComponent;
+    Camera playerCamera;
+
+    private void Awake()
+    {
+        this.movementComponent = this.gameObject.AddComponent<MovementComponent>();
+
+        Transform cameraTransform = this.transform.Find("Camera");
+        if(cameraTransform == null)
+        {
+            throw new System.Exception("The PlayerController does not have a Camera named 'Camera' in its children.");
+        }
+
+        this.playerCamera = cameraTransform.GetComponent<Camera>();
+
+        if (this.playerCamera == null)
+        {
+            throw new System.Exception("There is no Camera Component on the 'Camera' object.");
+        }
+    }
+}
